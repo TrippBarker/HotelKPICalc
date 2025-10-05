d9d8de7a58dd168ae0b2fad8d24564afd51cd679
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2025-10-05 12:47:24 -0400

    typos

--- calculator.js
+++ calculator.js
@@ -11,12 +11,12 @@ const occDisp = document.getElementById("occGraphDisp");
 const occGraph = document.getElementById("occGraph");
 
 function updateResults() {
-    // Remove all letters in form inputes with regex
+    // Remove all letters in form inputs with regex
     roomsAvailable.value = roomsAvailable.value.replace(/\D/g, "");
     roomsSold.value = roomsSold.value.replace(/\D/g, "");
     revenueIn.value = revenueIn.value.replace(/\D/g, "");
     let revVal = revenueIn.value;
-    revenueIn.value = "$" + (revVal / 100).toFixed(2)
+    revenueIn.value = "$" + (revVal / 100).toFixed(2);
     // Display the occupancy results, check for division by zero
     if (Number(roomsAvailable.value) > 0){
         occupancyResults.textContent = (roomsSold.value / roomsAvailable.value * 100).toFixed(2) + "%";
@@ -24,6 +24,8 @@ function updateResults() {
         occGraph.style.setProperty("--percent", (roomsSold.value / roomsAvailable.value * 100).toFixed(0));
     } else {
         occupancyResults.textContent = "0.00%"
+        occDisp.textContent = "0%"
+        occGraph.style.setProperty("--percent", 0);
     }
     // Display ADR, check for division by zero
     if (roomsSold.value > 0){
