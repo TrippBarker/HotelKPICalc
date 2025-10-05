549a4f66966f91684a67f8a58f3e0283e945fefd
Tripp Barker <105750441+TrippBarker@users.noreply.github.com>
2025-10-05 11:45:18 -0400

    adding calculations

--- calculator.js
+++ calculator.js
@@ -1,6 +1,40 @@
 const formInputs = document.querySelectorAll(".userIn");
 
+const roomsAvailable = document.getElementById("roomsAvailable");
+const roomsSold = document.getElementById("roomsSold");
+const revenueIn = document.getElementById("revenueIn");
+
+const occupancyResults = document.getElementById("occpancyResult");
+const adrResults = document.getElementById("adrResult");
+const revPARResults = document.getElementById("revPARResult");
+
+function updateResults() {
+    // Remove all letters in form inputes with regex
+    roomsAvailable.value = roomsAvailable.value.replace(/\D/g, "");
+    roomsSold.value = roomsSold.value.replace(/\D/g, "");
+    revenueIn.value = revenueIn.value.replace(/\D/g, "");
+    let revVal = revenueIn.value;
+    revenueIn.value = "$" + (revVal / 100).toFixed(2)
+    // Display the occupancy results, check for division by zero
+    if (Number(roomsAvailable.value) > 0){
+        occupancyResults.textContent = (roomsSold.value / roomsAvailable.value * 100).toFixed(2) + "%";
+    } else {
+        occupancyResults.textContent = "0.00%"
+    }
+
+    if (roomsSold.value > 0){
+        adrResults.textContent = "$" + ((revVal / 100) / roomsSold.value).toFixed(2);
+    } else {
+        adrResults.textContent = "$0.00";
+    }
+
+    if (roomsAvailable.value > 0){
+        revPARResults.textContent = "$" + ((revVal / 100) / roomsAvailable.value).toFixed(2);
+    } else {
+        adrResults.textContent = "$0.00";
+    }
+}
+
 formInputs.forEach(input => {
-    console.log(input.id);
-    
+    input.addEventListener("input", updateResults);
 })
